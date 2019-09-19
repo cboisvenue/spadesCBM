d66db780b0566f112071f76c3ad1f145c4e21134
Boisvenue <cboisven@nrn.nrcan.gc.ca>
2019-09-19 14:13:52 -0700

    cleaned some code

--- spadesCBMinputs/R/spadesCBMinputsFunctions.r
+++ spadesCBMinputs/R/spadesCBMinputsFunctions.r
@@ -168,31 +168,6 @@ convertM3biom <- function(meta,gCvalues,spsMatch,ecozones,params3, params4, para
 # END process growth curve functions-------------------------------------------------------------  
 
 
-# processGrowthCurve <- function(gcid,growthCurves,growthCurveComponents,sim) {
-#   
-#   matchingRows <- t(as.matrix(growthCurves[growthCurves[,"growth_curve_id"]==gcid,]))
-#   swSpeciesCode = 0
-#   hwSpeciesCode = 0
-#   swAgeVolumePairs = as.matrix(0)
-#   hwAgeVolumePairs = as.matrix(0)
-#   for(row in 1:nrow(matchingRows)){
-#     if(matchingRows[row,"forest_type_id"]==1) {
-#       swSpeciesCode <- matchingRows[row, "species_id"]
-#       swAgeVolumePairs <- growthCurveComponents[growthCurveComponents[,"GrowthCurveComponentID"] == matchingRows[row,"growth_curve_component_id"], c("Age","MerchVolume")]
-#     } else {
-#       hwSpeciesCode <- matchingRows[row, "species_id"]
-#       hwAgeVolumePairs <- growthCurveComponents[growthCurveComponents[,"GrowthCurveComponentID"] == matchingRows[row,"growth_curve_component_id"], c("Age","MerchVolume")]
-#     }
-#   }
-#   carbonCurve <- VolumeToBiomassConvert(dbpath = sim$dbPath, 
-#                                         spatial_unit_id = matchingRows[1,"spatial_unit_id"],
-#                                         sw_species_code = swSpeciesCode, 
-#                                         swAgeVolume = swAgeVolumePairs, 
-#                                         hw_species_code = hwSpeciesCode, 
-#                                         hwAgeVolume = hwAgeVolumePairs)
-#   return (carbonCurve)
-# }
-
 ### celine's disturbance magic
 spuDist <- function(mySpu = c(27,28),dbPath = file.path(getwd(),"data","cbm_defaults","cbm_defaults.db")){
   
