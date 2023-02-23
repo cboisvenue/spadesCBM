6bad42cc44ae01176862393345cfbc3472a03fe3
Alex Chubaty <achubaty@for-cast.ca>
2023-02-23 14:22:34 -0700

    add global preamble + update project seutp
    multiple workarounds currently needed using `Require` and `SpaDES.project`

--- new_libCBMGlobal.R
+++ new_libCBMGlobal.R
@@ -1,83 +1,142 @@
-# start of work to go from C++ to libcbm4
+## start of work to go from C++ to libcbm4
 # CBoisvenue
 # newGlobal
 
-## PFC work-around
-## this is a work-around for working from PFC...R cannot connect to URL
+# project basic setup -------------------------------------------------------------------------
 
-options("download.file.method" = "wininet")
+if (file.exists("~/.Renviron")) readRenviron("~/.Renviron") ## GITHUB_PAT
+if (file.exists("spadesCBM.Renviron")) readRenviron("spadesCBM.Renviron") ## database credentials
 
-while (!require("SpaDES.project")) {
-  install.packages("SpaDES.project", repos = "https://predictiveecology.r-universe.dev")
-  require(SpaDES.project)
+.ncores <- min(parallel::detectCores() / 2, 32L) ## default number of CPU cores to use, e.g. for pkg install
+.nodename <- Sys.info()[["nodename"]] ## current computer name; used to configure machine-specific settings
+.user <- Sys.info()[["user"]] ## current computer username; used to configure user-specific settings
+
+## define project directory - this code expects it is being run from this location
+## **do not change the paths defined here**
+## if you need to add a machine- or user-specific path, please do so _conditionally_
+prjDir <- switch(.user,
+                 cboisven = "C:/Celine/github/spadesCBM",
+                 "~/GitHub/spadesCBM")
+
+## ensure script being run from the project directory
+stopifnot(identical(normalizePath(prjDir), normalizePath(getwd())))
+
+options(
+  Ncpus = .ncores,
+  repos = c(PE = "https://predictiveecology.r-universe.dev/", ## latest PredictievEcology packages
+            SF = "https://r-spatial.r-universe.dev/",         ## latest sf and other spatial packages
+            CRAN = "https://cloud.r-project.org"),
+  Require.RPackageCache = "default" ## will use default package cache directory: `RequirePkgCacheDir()`
+)
+
+## work-around for working from PFC...R cannot connect to certain urls
+## TODO: improve conditional by only using wininet if *at* PFC, not just on a PFC machine
+if ((.Platform$OS.type == "windows") && grepl("[L|W]-VIC", .nodename)) {
+  options("download.file.method" = "wininet")
 }
 
-##TODO current set-up creates input/inputs and input/outputs folder...need to
-##figure out which we keep and make sure the "extras" are not created. The
-##Spades.project call creates the ones without the "s" at the end but thorughout
-##the code the ones with the "s" are used...but this might be only in the
-##specific module folders. Modules .Rmd use "outputs" but no code seems to use it.
-## Inputs with an "s" seems to be only used here in CBM_core (lines 305-317):
-# spinup <- function(sim) {
-#   io <- inputObjects(sim, currentModule(sim))
-#   objectNamesExpected <- io$objectName
-#   available <- objectNamesExpected %in% ls(sim)
-#   if (any(!available)) {
-#     stop(
-#       "The inputObjects for CBM_core are not all available:",
-#       "These are missing:", paste(objectNamesExpected[!available], collapse = ", "),
-#       ". \n\nHave you run ",
-#       paste0("spadesCBM", c("defaults", "inputs", "m3ToBiomass", "userDist"), collapse = ", "),
-#       "?"
-#     )
-#   }
-## Need to see if we can change that to "input".
+# install and load packages -------------------------------------------------------------------
 
-out <- SpaDES.project::setupProject(paths = list(projectPath = "C:/Celine/github/spadesCBM",
-                                          modulePath = "modules"),
-                             packages = c("googledrive", "devtools"),
-                             require =
-                               c("PredictiveEcology/reproducible@development (>= 1.2.16.9017)",
-                                 "PredictiveEcology/SpaDES.core@development (>= 1.1.0.9001)"),
-                             modules = c("PredictiveEcology/CBM_defaults@main",
-                                         "PredictiveEcology/CBM_dataPrep_SK@development",
-                                         "PredictiveEcology/CBM_vol2biomass@CBM_vol2biomass_SK",
-                                         "PredictiveEcology/CBM_core@main"
-                             )
+## use project-specific location for packages to avoid conflicts with other projects
+pkgDir <- file.path(tools::R_user_dir(basename(prjDir), "data"), "packages",
+                    version$platform, getRversion()[, 1:2])
+dir.create(pkgDir, recursive = TRUE, showWarnings = FALSE)
+.libPaths(pkgDir, include.site = FALSE)
+message("Using libPaths:\n", paste(.libPaths(), collapse = "\n"))
+
+## package installation only; do not load module packages until after install
+if (!"remotes" %in% rownames(installed.packages(lib.loc = .libPaths()[1]))) {
+  install.packages("remotes")
+}
+
+if (!"Require" %in% rownames(installed.packages(lib.loc = .libPaths()[1])) ||
+    packageVersion("Require", lib.loc = .libPaths()[1]) < "0.2.6") {
+  remotes::install_github("PredictiveEcology/Require@development")
+}
+
+library(Require)
+
+# setLinuxBinaryRepo() ## setup binary package installation for linux users; currently interferes with remotes installation
+
+## installs development branch of SpaDES.core and SpaDES.project from https://predictiveecology.r-universe.dev
+pkgsToInstall <- c("googledrive", "SpaDES.core", "SpaDES.project")
+#Install(pkgsToInstall), upgrade = FALSE, standAlone = TRUE) ## TODO: fails with spatial pkgs + quickPlot
+if (!all(pkgsToInstall %in% rownames(installed.packages(lib.loc = .libPaths()[1])))) {
+  install.packages(pkgsToInstall)
+}
+
+if (.user == "cboisven") {
+  ## TODO CBMutils does not seem to load - I am connected to the development branch of CBMutils
+  devtools::load_all("C:/Celine/github/CBMutils")
+} else {
+  ## TODO: Require fails to install
+  # Install("PredictiveEcology/CBMutils@development", dependencies = TRUE, standAlone = TRUE, upgrade = FALSE)
+  remotes::install_github("PredictiveEcology/CBMutils@development", repos = "https://cloud.r-project.org", upgrade = FALSE)
+}
+
+## project setup using SpaDES.project --------------------------------------------------------------
+
+## WORKAROUND: setupProject overwriting .gitignore (SpaDES.project#23)
+# file.copy(".gitignore", ".gitignore.orig")
+if (file.exists(".gitignore")) {
+  unlink(".gitignore")
+}
+out <- SpaDES.project::setupProject(
+  paths = list(projectPath = prjDir,
+               packagePath = pkgDir,
+               modulePath = "modules",
+               inputPath = "inputs",
+               outputPath = "outputs"),
+  packages = c(pkgsToInstall, "CBMutils"), ## TODO: Require fails to install packages
+  require = c("PredictiveEcology/reproducible@development (>= 1.2.16.9017)",
+              "PredictiveEcology/SpaDES.core@development (>= 1.1.0.9001)"),
+  modules = c("PredictiveEcology/CBM_defaults@main",
+              "PredictiveEcology/CBM_dataPrep_SK@development",
+              "PredictiveEcology/CBM_vol2biomass@CBM_vol2biomass_SK",
+              "PredictiveEcology/CBM_core@main"),
+  params = list(
+    CBM_defaults = list(
+      .useCache = TRUE
+    ),
+    CBM_dataPrep_SK = list(
+      .useCache = TRUE
+    ),
+    CBM_vol2biomass = list(
+      .useCache = TRUE
+    ),
+    CBM_core = list(
+      #.useCache = "init", #c(".inputObjects", "init")
+      .plotInterval = 1,
+      .plotInitialTime = 1990,
+      poolsToPlot = c("totalCarbon"),
+      spinupDebug = FALSE ## TODO: temporary
+    )
+  ),
+  options = list(
+    reproducible.destinationPath = "inputs", ## TODO: SpaDES.project#24
+    reproducible.useTerra = TRUE
+  ),
+  times = list(start = 1990.00, end = 1993.00),
+  setLinuxBinaryRepo = FALSE, ## TODO: interferes with other package installation
+  updateRprofile  = FALSE ## TODO: verify what it is doing
 )
+if (!file.exists(".gitignore")) {
+  file.copy(".gitignore.orig", ".gitignore")
+}
 
+## simulation setup --------------------------------------------------------------------------------
 
-times <- list(start = 1990.00, end = 1993.00)
-parameters <- list(
-  CBM_defaults = list(
-    .useCache = TRUE
-  ),
-  CBM_dataPrep_SK = list(
-    .useCache = TRUE
-  ),
-  CBM_vol2biomass = list(
-    .useCache = TRUE
-  ),
-  CBM_core = list(
-    #.useCache = "init", #c(".inputObjects", "init")
-    .plotInterval = 1,
-    .plotInitialTime = 1990,
-    poolsToPlot = c("totalCarbon"),
-    spinupDebug = FALSE ## TODO: temporary
-  )
-)
+library(SpaDES.core)
 
 objects <- list(
-  dbPath = file.path(out$paths$modulePath, "CBM_defaults", "data","cbm_defaults", "cbm_defaults.db"),
-  sqlDir = file.path(out$paths$modulePath, "CBM_defaults", "data", "cbm_defaults")
+  dbPath = file.path(out$paths$inputPath, "cbm_defaults", "cbm_defaults.db"),
+  sqlDir = file.path(out$paths$inputPath, "CBM_defaults", "data", "cbm_defaults")
 )
 
 # quickPlot::dev.useRSGD(FALSE)
 # dev()
 # clearPlot()
-##TODO SpaDES.core is in the "require = " part of the setupProject call above.
-##Why is it not loading?
-library(SpaDES.core)
+
 ##TODO CBMutils does not seem to load
 # ::gcidsCreate
 # Error: 'gcidsCreate' is not an exported object from 'namespace:CBMutils'
@@ -89,22 +148,17 @@ library(SpaDES.core)
 # gcidsCreate <- function(...) {
 #   do.call(paste, c(list(...)[[1]], sep= "_"))
 # }
-##TODO added "devtools" to the setupProject packages =
-library("devtools")
-devtools::load_all("C:/Celine/github/CBMutils")
 
 spadesCBMrunsSK <- simInitAndSpades(times = times,
                                     params = parameters,
                                     modules = out$modules,
                                     objects = objects,
-                                    #paths = paths, ## not needed when using
-                                    #setPaths() above
                                     loadOrder = unlist(out$modules),
                                     debug = TRUE
 )
 
 
-## scrap
+## scrap -------------------------------------------------------------------------------------------
 
 install.packages("reproducible", repos = "https://predictiveecology.r-universe.dev")
 
