3bde31efa3de350c79f4c1a71bc4302e9ee4b365
Celine <cboisvenue@gmail.com>
2023-09-12 15:22:09 -0700

    update various globals

--- CBM_coreGlobal00.R
+++ CBM_coreGlobal00.R
@@ -20,15 +20,30 @@
 # }
 #options("download.file.method" = "wininet")
 #options(repos = "https://cloud.r-project.org")
-if (all(tryCatch(packageVersion("SpaDES.project") < "0.0.7.9023", error = function(e) TRUE),
-        tryCatch(packageVersion("Require") < "0.3.0", error = function(e) TRUE))) {
-  install.packages(c("Require", "SpaDES.project"),
-                   repos = c("https://predictiveecology.r-universe.dev",
-                             getOption("repos")))
-}
 
+##TODO work being done on SpaDES.project, this will need to be changed
 
+## This is not working
+# install.packages("Require")#, lib = 'C:/Users/cboisven/AppData/Local/R/win-library/4.3')
+#
+# Require::Require("PredictiveEcology/SpaDES.project@transition")
 
+## this is me trying
+library(remotes)
+remotes::install_github("PredictiveEcology/SpaDES.project@transition")
+#remotes::install_github("PredictiveEcology/reproducible@development")
+
+## below is what I used to use...
+# if (all(tryCatch(packageVersion("SpaDES.project") < "0.0.7.9023", error = function(e) TRUE),
+#         tryCatch(packageVersion("Require") < "0.3.0", error = function(e) TRUE))) {
+#   install.packages(c("Require", "SpaDES.project"),
+#                    repos = c("https://predictiveecology.r-universe.dev",
+#                              getOption("repos")))
+# }
+
+library(SpaDES.project)
+
+times <- list(start = 1998, end = 1999)
 out <- SpaDES.project::setupProject(
   name = "spadesCBM",
   paths = list(projectPath =
@@ -43,53 +58,77 @@ out <- SpaDES.project::setupProject(
               CRAN = "https://cloud.r-project.org"),
     reproducible.destinationPath = "inputs", ## TODO: SpaDES.project#24
     reproducible.useTerra = TRUE,
-    reproducible.rasterRead = "terra::rast"
+    reproducible.rasterRead = "terra::rast",
+    ## These are for speed
+    reproducible.useMemoise = TRUE,
+    # Require.offlineMode = TRUE,
+    spades.moduleCodeChecks = FALSE
   ),
   params = list(
-    # CBM_defaults = list(
-    #   .useCache = TRUE
-    # ),
-    # CBM_dataPrep_SK = list(
-    #   .useCache = TRUE
-    # ),
-    # CBM_vol2biomass = list(
-    #   outputFigurePath = switch(user(),
-    #                             achubaty =
-    #                               file.path("outputs", "figures", "CBM_vol2biomass"),
-    #                             NA), ## NA means use default: './modules/CBM_vol2biomass/figures/'
-    #   .plotInitialTime = 1990,
-    #   .useCache = TRUE
-    # ),
+    CBM_defaults = list(
+      .useCache = TRUE
+    ),
+    CBM_dataPrep_SK = list(
+      .useCache = TRUE
+    ),
+    CBM_vol2biomass = list(
+      outputFigurePath = switch(user(),
+                                achubaty =
+                                  file.path("outputs", "figures", "CBM_vol2biomass"),
+                                NA), ## NA means use default: './modules/CBM_vol2biomass/figures/'
+      .plotInitialTime = times$start,
+      .useCache = TRUE
+    ),
     CBM_core = list(
       #.useCache = "init", #c(".inputObjects", "init")
       .plotInterval = 1,
-      .plotInitialTime = 1990,
+      .plotInitialTime = times$start,
       poolsToPlot = c("totalCarbon"),
       spinupDebug = FALSE ## TODO: temporary
     )
   ),
-  # packages = "pkgload",
-   require = c(
-  #   c("googledrive",
-  #     "PredictiveEcology/reproducible@development", #development (>= 1.2.16.9017)",
-       "PredictiveEcology/SpaDES.core@development (>= 1.1.1)"# ,
-  #     "PredictiveEcology/CBMutils@development (HEAD)"
+  packages = "pkgload",
+  require =
+    c("googledrive",
+      "PredictiveEcology/reproducible@development (>= 2.0.8.9001)", ##fixed a problem with caching dataPrep
+      "PredictiveEcology/SpaDES.core@useCache2 (>= 2.0.2.9003)",
+      "CBMutils"),
+  modules = c("PredictiveEcology/CBM_defaults",
+              "PredictiveEcology/CBM_dataPrep_SK",
+              "PredictiveEcology/CBM_vol2biomass",
+              "PredictiveEcology/CBM_core"
   ),
-  modules = c("PredictiveEcology/CBM_defaults@main",
-    #"PredictiveEcology/CBM_dataPrep_SK@development",
-    #"PredictiveEcology/CBM_vol2biomass@CBM_vol2biomass_SK",
-    "PredictiveEcology/CBM_core@runCoreAlone"
-  ),
-  times = list(start = 1990.00, end = 1993.00),
+  times = times,
   setLinuxBinaryRepo = FALSE, ## TODO: interferes with other package installation
-  updateRprofile  = FALSE ## TODO: verify what it is doing
+  updateRprofile  = FALSE, ## TODO: verify what it is doing
+  sqlDir = file.path(out$paths$modulePath, "CBM_defaults", "data", "cbm_defaults"),
+  dbPath = file.path(sqlDir, "cbm_defaults.db")
 )
 
+##TODO: At the end of each annual event, sim$cbmPools is replaced by updated
+## pools where all the carbon transactions are done for that year and pools
+## updated.However, because annual inter variability is useful for carbon
+## dynamics, the default behaviour in this example is for to write yearly
+## results to sim$$outputPath via the out$outputs below. This may become really
+## big for large of fine-pixelled areas. Need to create a user-controlled
+## parameter for "# of years saved". Note that the second to last sim$cbmPools
+## needs to be saved if we want to be able to use the
+## CBMutils::checkTransactions() function. Maybe make that saving a default?
+
+
+#out$debug = 1 is the default which is like TRUE but not quite
+out$loadOrder <- unlist(out$modules)
 
 # This line is when is to locally load the CBMutils package, to debug or modify
 # CBMutils functions
 # pkgload::load_all("C:\\Celine\\github\\CBMutils")
-out$loadOrder <- unlist(out$modules)
+
+##TODO: deal with plotting issues. Plotting takes longer than the simulation,
+## and if the window is not big enough, the simulation will crash. If you open a
+## window (as the lines below do) the sim will run.
+# quickPlot::dev.useRSGD(FALSE)
+# dev()
+# clearPlot()
 
 # creating a data.frame to save $cbmPools and $NPP at specific time steps.
 out$outputs <- as.data.frame(expand.grid(objectName = c("cbmPools", "NPP"),
@@ -97,8 +136,7 @@ out$outputs <- as.data.frame(expand.grid(objectName = c("cbmPools", "NPP"),
                                                            out$times$start +
                                                              c(1:(out$times$end - out$times$start))
                                          ))))
-# dev()
-coreCBMrunsSK <- do.call(simInitAndSpades, out)
+#dev()
 
 #   simInitAndSpades(times = out$times,
 #                                     params = parameters,
@@ -115,66 +153,19 @@ coreCBMrunsSK <- do.call(simInitAndSpades, out)
 
 
 
-if (FALSE) {
+extent <- reproducible::.unwrap(structure(list(xmin = -687696, xmax = -681036,
+                                               ymin = 711955, ymax = 716183), class = "PackedSpatExtent"))
+masterRaster <- terra::rast(extent, res = 30)
+terra::crs(masterRaster) <- "PROJCRS[\"Lambert_Conformal_Conic_2SP\",\n    BASEGEOGCRS[\"GCS_GRS_1980_IUGG_1980\",\n        DATUM[\"D_unknown\",\n            ELLIPSOID[\"GRS80\",6378137,298.257222101,\n                LENGTHUNIT[\"metre\",1,\n                    ID[\"EPSG\",9001]]]],\n        PRIMEM[\"Greenwich\",0,\n            ANGLEUNIT[\"degree\",0.0174532925199433,\n                ID[\"EPSG\",9122]]]],\n    CONVERSION[\"Lambert Conic Conformal (2SP)\",\n        METHOD[\"Lambert Conic Conformal (2SP)\",\n            ID[\"EPSG\",9802]],\n        PARAMETER[\"Latitude of false origin\",49,\n            ANGLEUNIT[\"degree\",0.0174532925199433],\n            ID[\"EPSG\",8821]],\n        PARAMETER[\"Longitude of false origin\",-95,\n            ANGLEUNIT[\"degree\",0.0174532925199433],\n            ID[\"EPSG\",8822]],\n        PARAMETER[\"Latitude of 1st standard parallel\",49,\n            ANGLEUNIT[\"degree\",0.0174532925199433],\n            ID[\"EPSG\",8823]],\n        PARAMETER[\"Latitude of 2nd standard parallel\",77,\n            ANGLEUNIT[\"degree\",0.0174532925199433],\n            ID[\"EPSG\",8824]],\n        PARAMETER[\"Easting at false origin\",0,\n            LENGTHUNIT[\"metre\",1],\n            ID[\"EPSG\",8826]],\n        PARAMETER[\"Northing at false origin\",0,\n            LENGTHUNIT[\"metre\",1],\n            ID[\"EPSG\",8827]]],\n    CS[Cartesian,2],\n        AXIS[\"easting\",east,\n            ORDER[1],\n            LENGTHUNIT[\"metre\",1,\n                ID[\"EPSG\",9001]]],\n        AXIS[\"northing\",north,\n            ORDER[2],\n            LENGTHUNIT[\"metre\",1,\n                ID[\"EPSG\",9001]]]]"
+masterRaster[] <- rep(1, terra::ncell(masterRaster))
+mr <- prepInputs(url = "https://drive.google.com/file/d/1zUyFH8k6Ef4c_GiWMInKbwAl6m6gvLJW",
+                 to = masterRaster,
+                 method = "near")
+mr[mr[] == 0] <- NA
 
-  # CBM_core global for Scott to start python transition
-  library(remotes)
-  remotes::install_github("PredictiveEcology/SpaDES.core@development")
-  # remotes::install_github("PredictiveEcology/SpaDES.tools@development")
-  # remotes::install_github("PredictiveEcology/reproducible@development")
-  # remotes::install_github("PredictiveEcology/Require@development")
+out$objects$masterRaster <- mr
 
-  library(SpaDES.tools)
-  library(SpaDES.core)
-  library(Require)
-  #library(SpaDES.project)
+options("spades.allowInitDuringSimInit" = TRUE)
+# out$events <- c("init")
+spadesCBMrunsSK <- do.call(simInitAndSpades, out)
 
-  #outCore <- SpaDES.project::setupProject(
-
-  projectPath = getwd()
-
-  paths = list(
-    cachePath = file.path(projectPath, "cache"),
-    inputPath = file.path(projectPath, "inputs"),
-    modulePath = file.path(projectPath, "modules"),
-    outputPath = file.path(projectPath, "outputs")
-  )
-
-
-  options = list(
-    repos = c(PE = "https://predictiveecology.r-universe.dev/", ## latest PredictievEcology packages
-              SF = "https://r-spatial.r-universe.dev/",         ## latest sf and other spatial packages
-              CRAN = "https://cloud.r-project.org"),
-    reproducible.destinationPath = "inputs", ## TODO: SpaDES.project#24
-    reproducible.useTerra = TRUE,
-    reproducible.rasterRead = "terra::rast"
-  )
-
-  packages = "pkgload"
-  require =
-    c("googledrive",
-      "PredictiveEcology/reproducible@development", #development (>= 1.2.16.9017)",
-      "PredictiveEcology/SpaDES.core@development (>= 1.1.1)",
-      "PredictiveEcology/CBMutils@development (HEAD)")
-  modules = c("PredictiveEcology/CBM_core@runCoreAlone")
-  times = list(start = 1990.00, end = 1993.00)
-
-  parameters <- list(
-    CBM_core = list(.useCache = ".inputObjects"))
-  #.progress = list(type = "text", interval = 1), # for a progress bar
-  ## If there are further modules, each can have its own set of parameters:
-  #module1 = list(param1 = value1, param2 = value2),
-  #module2 = list(param1 = value1, param2 = value2)
-  modules <- "CBM_core"
-
-
-
-  myCore <- simInit( times = times,
-                     params = parameters,
-                     modules = modules,
-                     paths = paths
-  )
-
-  outCore <- spades(myCore, debug = TRUE)
-
-}
