fac6183df41691b22627123a0872383e4079cf3a
Celine Boisvenue <cboisvenue@gmail.com>
2024-06-26 16:18:54 -0700

    Python environment stuff

--- globalCore1.R
+++ globalCore1.R
@@ -34,10 +34,6 @@ install.packages(c("Require", "SpaDES.project"), repos = repos)
 # start in 1998 because there are known disturbances in the study area
 times <- list(start = 1998, end = 2000)
 
-##TODO the out will not run without me loading reproducible...I thought it would
-##do it through the SpaDES.project::setupProject fnct. This is a workaround
- library(reproducible)
-
 out <- SpaDES.project::setupProject(
   name = "spadesCBM",
   ##TODO need to figure out how to connect a new modules and new repo
@@ -57,7 +53,10 @@ out <- SpaDES.project::setupProject(
   ),
   modules =  "PredictiveEcology/CBM_core@python", ##TODO not linked yet!
   times = times,
-  require = "PredictiveEcology/SpaDES.core@development",
+  require = c("PredictiveEcology/SpaDES.core@development",
+              "reticulate",
+              "PredictiveEcology/libcbmr"),
+
   ####M begin manually passed inputs ####
   functions = "PredictiveEcology/spadesCBM@libCBMtransition/R/temporaryFuns.R",
   processes = readRDS(file.path(paths$inputPath, "processes.rds")),
@@ -140,21 +139,26 @@ out <- SpaDES.project::setupProject(
   disturbanceRasters = {
     rasts <- terra::rast(file.path(paths$inputPath, paste0("SaskDist_", times$start:times$end, ".grd")))
     names(rasts) <- times$start:times$end
-    rasts <- postProcess2(rasts, cropTo = masterRaster, projectTo = masterRaster,
+    rasts <- postProcessTo(rasts, cropTo = masterRaster, projectTo = masterRaster,
                               maskTo = masterRaster, method = "near")
   },
   gc_df = make_gc_df(readRDS(file.path(paths$inputPath, "gcHash.rds"))),
+  Restart = getOption("SpaDES.project.Restart", FALSE),
 
   outputs = as.data.frame(expand.grid(objectName = c("cbmPools", "NPP"),
                                       saveTime = sort(c(times$start,
                                                         times$start +
                                                           c(1:(times$end - times$start))
                                       )))),
-  updateRprofile = TRUE
+  updateRprofile = TRUE,
+  reticulate::use_virtualenv(virtualenv = "r-reticulate"),
+  reticulate::py_install("numpy==1.26.4", envname = "r-reticulate"),
+  reticulate::py_install("pandas==2.2.2", envname = "r-reticulate"),
+  reticulate::py_install("libcbm", envname = "r-reticulate")
 )
 
 ## if you don't have CBMutils, you can get it here "PredictiveEcology/CBMutils"
-devtools::load_all("../libcbmr")
+#devtools::load_all("../libcbmr")
 #install_libcbm(method = "virutalenv")
 # Error in basename(condaenv) : a character vector argument expected
 # In addition: Warning message:
@@ -168,17 +172,38 @@ devtools::load_all("../libcbmr")
 
 ##TODO do we need to install libcbm? (this is a python package) doesn't having
 ##libcbmr suffice? don't we have to do it like this?
-library(reticulate)
+# library(reticulate)
+# reticulate::use_virtualenv(virtualenv = "r-reticulate")
 ###CELINE NOTES: the following line takes a long time...do we need it?? or is it
 ###a check to make sure things are as they should be?
-reticulate::import("sys")$executable
+#reticulate::import("sys")$executable
 #[1] "C:\\Users\\cboisven\\AppData\\Local\\R-MINI~1\\envs\\R-RETI~1\\python.exe"
-libcbm <- reticulate::import("libcbm")
+###CELINE NOTES: this next line was to revert to an older version of numpy
+###Python package. The June 16th update to version 2.0 created some issues.
+###Scott had the same error, error pasted here.
+# source("C:/Users/smorken/dev/code/spades/libcbmr/tests/spatial_integration_test/run_spatial_test.R") Show Traceback Rerun with DebugError in py_call_impl(callable, call_args$unnamed, call_args$named) :
+#   ValueError: If using all scalar values, you must pass an indexRun `reticulate::py_last_error()` for details.
+# reticulate::py_install("numpy<2.0", envname = "r-reticulate", ignore_installed=TRUE)
+#libcbm <- reticulate::import("libcbm")
+
+### other possible ways if the above stopped working, ran this instead:
+
+#print(reticulate::py_get_attr(libcbm, "__version__"))
+#install the latest numpy < 2.0
+#
+# reticulate::use_virtualenv(virtualenv = "r-reticulate")
+# reticulate::py_install("numpy==1.26.4", envname = "r-reticulate")
+# reticulate::py_install("pandas==2.2.2", envname = "r-reticulate")
+#
+# #put this last: because when you install libcbm, it will automatically install all of its dependencies, potentially overwriting your custom-selected pacakge versions
+# reticulate::py_install("libcbm", envname = "r-reticulate")
+##check things with
+# py_config() ## this is confusing!!
+
+
 
 out$cbmData = readRDS(file.path(out$paths$inputPath, "cbmData.rds"))
-##TODO smae as for reproducible...SpaDES.core is not installed??
-# install.packages("SpaDES.core", repos = repos)
-# library("SpaDES.core")
+
 # Run
 simPython <- do.call(SpaDES.core::simInitAndSpades, out)
 
